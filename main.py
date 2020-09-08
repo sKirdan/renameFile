c668626ddb47080ef513625ecf34f695f31cc78d
User <rex1532007@gmail.com>
2020-09-08 14:53:18 +0300

    rename file organizer

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+link: str = "C:\\Users\\User\Desktop\osTest"
+
+
+# print(os.getcwd())
+
+
+def app(str1: str):
+    os.chdir(link)
+    name: int = 1
+    for file in os.listdir(str1):
+        print(file)
+        src = os.path.abspath(file)
+        dst = str1 + '\\' + str(name) + "." + os.path.basename(file).split(".")[-1]
+        print(src)
+        print(dst)
+
+        os.rename(src, dst)
+        name += 1
+        print(name)
+
+
+app("C:\\Users\\User\Desktop\osTest")
